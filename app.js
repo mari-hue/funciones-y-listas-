a781025a21f9b22d23b20b8aabe09b799a509b5a
Maria <vasquezms@hotmail.com>
2025-02-13 18:39:01 -0300

    Nueva Actualizacion, y desafios

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-let numeroSecreto = generarNumeroSecreto();
+let numeroSecreto = 0; 
+let intentos = 0;
+console.log(numeroSecreto);
 
 function asignarTextoElementos(elemento, texto){
     let elementoHtml = document.querySelector(elemento);
@@ -7,19 +9,50 @@ function asignarTextoElementos(elemento, texto){
 
 function verificarIntento(){
     let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
-    console.log(typeof(numeroDeUsuario));
-    console.log(numeroSecreto);
-    console.log(typeof(numeroSecreto));
-    console.log(numeroDeUsuario);   
-    console.log(numeroDeUsuario === numeroSecreto);
+    console.log(intentos);
+    if (numeroDeUsuario === numeroSecreto) {
+        asignarTextoElementos('p', `¡Felicidades! ¡Acertaste el número! en ${intentos} ${(intentos === 1) ? 'vez' : 'Veces'}`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if (numeroDeUsuario > numeroSecreto){
+            asignarTextoElementos('p ', 'El número secreto es menor');
+        } else {
+            asignarTextoElementos('p', 'El número secreto es mayor');
+        }
+        intentos ++;
+        limpiarCaja();
+    }
     return;
 }
+
+function limpiarCaja() {
+    document.querySelector('#valorUsuario').value ='';
+}
+
 
 function generarNumeroSecreto() {
     return Math.floor(Math.random()*10)+1; 
 }
 
+function condicionesIniciales(){
+    asignarTextoElementos('h1', "Juego del número secreto");
+    asignarTextoElementos('p', "elige un numero del 1 al 10");
+    numeroSecreto = generarNumeroSecreto();
+    intentos =1 ;
+
+}
+function reiniciarJuego() {
+    //limpiar caja
+    limpiarCaja();
+    //Indicar mensaje de intervalos de numeros
+    //Generar numero aleatorio
+    //Inicializar el numero de intentos
+    condicionesIniciales();
+    //Deshabilitar el boton de nuevo juego
+    document.querySelector('#reiniciar').setAttribute('disabled', 'true');
+
+}
+    condicionesIniciales();
 
 
-asignarTextoElementos('h1', "Juego del número secreto");
-asignarTextoElementos('p', "elige un numero del 1 al 10");
+
--- app1.js
+++ app1.js
@@ -26,3 +26,28 @@ function promedio (nota1,nota2,nota3){
 
 let promedioFinal = promedio (3,3,3);
 console.log(promedioFinal);
+
+function numeroMayor(a,b){
+    if (a > b) {
+        console.log(a + 'es mayor que ' + b);
+    }else {
+        console.log(b + ' es mayor que ' + a);
+    }
+}
+numeroMayor(1,9);
+
+function multiplicarSimismo (a){
+    return (a*a);
+}
+let resultadomulti = multiplicarSimismo (3);
+console.log(` El resultado de la multiplicación es ${resultadomulti}`);
+
+
+function calculaImc (peso, altura){
+    return peso / (altura*altura);
+}
+var imc = calculaImc(69,1.67);  
+
+calculaImc(69, 1.67);
+console.log( `Tu IMC es ${imc.toFixed(2)}`);
+
